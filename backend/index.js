0658d09837675c9e8d1a109fc43af496b3e2b00b
ursachecodrut <ursache.codrut71@gmail.com>
2021-09-09 20:12:53 +0300

    add simple upload image feature

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+let cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -28,6 +32,22 @@ app.post('/', (req, res) => {
 	runPy(req.body.image).then((fromRunPy) => {
 		console.log(fromRunPy.toString());
 		res.end(fromRunPy);
+	});
+});
+
+app.post('/upload', (req, res) => {
+	if (req.files === null) {
+		return res.status(400).json({ msg: 'No file was uploaded' });
+	}
+
+	const file = req.files.file;
+
+	file.mv(`${__dirname}/../frontend/public/uploads/${file.name}`, (err) => {
+		if (err) {
+			console.error(err);
+			return res.status(500).send(err);
+		}
+		res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
 	});
 });
 
