54702362e17138044c259868fd71b79c5aa47d4c
Codrut Ursache <ursache.codrut71@gmail.com>
2023-02-20 13:42:16 +0200

    fix dependencies

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -4,95 +4,95 @@ import Progress from './Progress';
 import axios from 'axios';
 
 const FileUpload = () => {
-	const [file, setFile] = useState('');
-	const [filename, setFilename] = useState('Choose File');
-	const [uploadedFile, setUploadedFile] = useState({});
-	const [message, setMessage] = useState('');
-	const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
+  const [uploadPercentage, setUploadPercentage] = useState(0);
 
-	const onChange = (e) => {
-		setFile(e.target.files[0]);
-		setFilename(e.target.files[0].name);
-	};
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
 
-	const onSubmit = async (e) => {
-		e.preventDefault();
-		const formData = new FormData();
-		formData.append('file', file);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
 
-		try {
-			const res = await axios.post('/upload', formData, {
-				headers: {
-					'Content-Type': 'multipart/form-data',
-				},
-				onUploadProgress: (progressEvent) => {
-					setUploadPercentage(
-						parseInt(
-							Math.round((progressEvent.loaded * 100) / progressEvent.total)
-						)
-					);
-				},
-			});
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => {
+          setUploadPercentage(
+            parseInt(
+              Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            )
+          );
+        },
+      });
 
-			// Clear percentage
-			setTimeout(() => setUploadPercentage(0), 10000);
+      // Clear percentage
+      setTimeout(() => setUploadPercentage(0), 10000);
 
-			const { fileName, filePath } = res.data;
+      const { fileName, filePath } = res.data;
 
-			setUploadedFile({ fileName, filePath });
+      setUploadedFile({ fileName, filePath });
 
-			setMessage('File Uploaded');
-		} catch (err) {
-			if (err.response.status === 500) {
-				setMessage('There was a problem with the server');
-			} else {
-				setMessage(err.response.data.msg);
-			}
-			setUploadPercentage(0);
-		}
-	};
+      setMessage('File Uploaded');
+    } catch (err) {
+      if (err.response.status === 500) {
+        setMessage('There was a problem with the server');
+      } else {
+        setMessage(err.response.data.msg);
+      }
+      setUploadPercentage(0);
+    }
+  };
 
-	return (
-		<Fragment>
-			{message ? <Message msg={message} /> : null}
-			<form onSubmit={onSubmit}>
-				<div className="custom-file mb-4">
-					<input
-						type="file"
-						className="custom-file-input"
-						id="customFile"
-						onChange={onChange}
-					/>
-					<label className="custom-file-label" htmlFor="customFile">
-						{filename}
-					</label>
-				</div>
+  return (
+    <Fragment>
+      {message ? <Message msg={message} /> : null}
+      <form onSubmit={onSubmit}>
+        <div className='custom-file mb-4'>
+          <input
+            type='file'
+            className='custom-file-input'
+            id='customFile'
+            onChange={onChange}
+          />
+          <label className='custom-file-label' htmlFor='customFile'>
+            {filename}
+          </label>
+        </div>
 
-				<Progress percentage={uploadPercentage} />
+        <Progress percentage={uploadPercentage} />
 
-				<input
-					type="submit"
-					value="Upload"
-					className="btn btn-primary btn-block mt-4"
-				/>
-				{uploadPercentage === 100 ? (
-					<button className="btn btn-success btn-block mt-4">
-						<a href="/htmlGen/index.html" style={{ color: 'white' }} download>
-							Click to download html file
-						</a>
-					</button>
-				) : null}
-			</form>
-			{uploadedFile ? (
-				<div className="row mt-5">
-					<div className="col-md-6 m-auto">
-						<h3 className="text-center">{uploadedFile.fileName}</h3>
-						<img style={{ width: '100%' }} src={uploadedFile.filePath} alt="" />
-					</div>
-				</div>
-			) : null}
-		</Fragment>
-	);
+        <input
+          type='submit'
+          value='Upload'
+          className='btn btn-primary btn-block mt-4'
+        />
+        {uploadPercentage === 100 ? (
+          <button className='btn btn-success btn-block mt-4'>
+            <a href='/htmlGen/index.html' style={{ color: 'white' }} download>
+              Click to download html file
+            </a>
+          </button>
+        ) : null}
+      </form>
+      {uploadedFile ? (
+        <div className='row mt-5'>
+          <div className='col-md-6 m-auto'>
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+          </div>
+        </div>
+      ) : null}
+    </Fragment>
+  );
 };
 
 export default FileUpload;
